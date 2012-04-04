a94193dd05ad3bfc4c5043e395cc0bb9eb2cd90e
Benoit Chesneau <bchesneau@gmail.com>
2012-04-04 04:15:22 +0200

    sync with couch

--- apps/couch_httpd/test/javascript/cli_runner.js
+++ apps/couch_httpd/test/javascript/cli_runner.js
@@ -13,40 +13,47 @@
 var console = {
   log: function(arg) {
     var msg = (arg.toString()).replace(/\n/g, "\n    ");
-    print("# " + msg);
+    print(msg, true);
   }
 };
 
 function T(arg1, arg2) {
   if(!arg1) {
-    throw((arg2 ? arg2 : arg1).toString());
+    var result = (arg2 ? arg2 : arg1);
+    throw((result instanceof Error ? result : Error(result)));
   }
 }
 
 function runTestConsole(num, name, func) {
+  var passed = false;
   try {
     func();
+    passed = true;
     print("ok " + num + " " + name);
   } catch(e) {
-    msg = e.toString();
-    msg = msg.replace(/\n/g, "\n    ");
-    print("not ok " + num + " " + name + " " + msg);
+    print("not ok " + num + " " + name);
+    console.log(e.toSource());
+    if (e.stack) {
+      console.log("Stacktrace:\n" + e.stack.replace(/^/gm, "\t"));
+    }
   }
+  return passed;
 }
 
 function runAllTestsConsole() {
   var numTests = 0;
+  var numPassed = 0;
   for(var t in couchTests) { numTests += 1; }
   print("1.." + numTests);
   var testId = 0;
   for(var t in couchTests) {
     testId += 1;
-    runTestConsole(testId, t, couchTests[t]);
+    if(runTestConsole(testId, t, couchTests[t])) {
+      numPassed++;
+    }
   }
+  T(numPassed == numTests, "All JS CLI tests should pass.");
 };
 
-try {
-  runAllTestsConsole();
-} catch (e) {
-  p("# " + e.toString());
-}
+waitForSuccess(CouchDB.getVersion);
+runAllTestsConsole();
