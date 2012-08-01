d08091c3e8339a56e465e0a8689334e7d59ace88
benoitc <bchesneau@gmail.com>
2012-08-01 09:28:47 +0200

    fix include

--- apps/couch_mrview/src/couch_mrview.erl
+++ apps/couch_mrview/src/couch_mrview.erl
@@ -23,7 +23,7 @@
 -export([cleanup/1]).
 
 -include_lib("couch/include/couch_db.hrl").
--include_lib("couch_mrview/include/couch_mrview.hrl").
+-include("couch_mrview.hrl").
 
 -record(mracc, {
     db,
