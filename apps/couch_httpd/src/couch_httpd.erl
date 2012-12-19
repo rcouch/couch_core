ea78d31e60aa467673505d84cbf003856c30d06e
Robert Newson <rnewson@apache.org>
2012-12-19 00:44:02 +0000

    Include a comment before jsonp output

--- apps/couch_httpd/src/couch_httpd.erl
+++ apps/couch_httpd/src/couch_httpd.erl
@@ -797,7 +797,7 @@ start_jsonp() ->
     case get(jsonp) of
         no_jsonp -> [];
         [] -> [];
-        CallBack -> CallBack ++ "("
+        CallBack -> ["/* CouchDB */", CallBack, "("]
     end.
 
 end_jsonp() ->
